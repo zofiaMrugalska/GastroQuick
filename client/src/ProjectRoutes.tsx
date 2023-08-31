cc701bce41dd2665753d0cf7bcd6a89ae033ad52
Zosia <zosiagrafika11@gmail.com>
2023-08-31 23:33:16 +0200

    account route

--- client/src/ProjectRoutes.tsx
+++ client/src/ProjectRoutes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import User from "./pages/account/User";
 import Auth from "./pages/authentication/Auth";
 import Menu from "./pages/home/Menu";
 import Cart from "./pages/order/Cart";
@@ -13,6 +14,7 @@ const ProjectRoutes = () => {
           <Route path="/" element={<Menu />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/authentication" element={<Auth />} />
+          <Route path="/account" element={<User />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
 import { LiaShoppingCartSolid } from "react-icons/lia";
+import { FiUser } from "react-icons/fi";
+import { BiLogInCircle, BiFoodMenu } from "react-icons/bi";
 
 const Navbar = () => {
   return (
@@ -8,16 +10,20 @@ const Navbar = () => {
       <nav className="flex justify-between">
         <h1 className=" text-3xl font-medium">GastroQuick</h1>
         <ul className="flex items-center gap-5">
-          <Link to={"/"} className=" text-xl font-semibold">
-            MENU
+          <Link to={"/"}>
+            <BiFoodMenu size={25} />
           </Link>
 
           <Link to={"/cart"}>
-            <LiaShoppingCartSolid size={37} />
+            <LiaShoppingCartSolid size={35} />
           </Link>
 
-          <Link to={"/authentication"} className=" text-xl font-semibold">
-            LOG IN
+          <Link to={"/account"}>
+            <FiUser size={25} />
+          </Link>
+
+          <Link to={"/authentication"}>
+            <BiLogInCircle size={25} />
           </Link>
         </ul>
       </nav>
