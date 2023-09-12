d532e2a9c9f0dbbfb1f4025e4e4f8efef735c5be
Zosia <zosiagrafika11@gmail.com>
2023-09-12 11:20:10 +0200

    user/meal model and mongo connect

--- server/src/models/mealModel.js
+++ server/src/models/mealModel.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 
-const mealSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const mealSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    jpg: {
+      type: String,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  jpg: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const mealModel = mongoose.model("Meal", mealSchema);
 
--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "please add the name"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please add the name"],
+    },
+    email: {
+      type: String,
+      required: [true, "please add the email"],
+      unique: [true, "email address already taken"],
+    },
+    password: {
+      type: String,
+      required: [true, "plase add the password"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "please add the email"],
-    unique: [true, "email address already taken"],
-  },
-  password: {
-    type: String,
-    required: [true, "plase add the password"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.model("User", userSchema);
 
