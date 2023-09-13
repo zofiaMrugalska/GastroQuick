ff9a4743e3c5b97879e9c5751fdefb48646ca981
Zosia <zosiagrafika11@gmail.com>
2023-09-13 14:54:47 +0200

    add response dto

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const userModel = require("../models/userModel");
+const createResponse = require("../services/responseDTO");
 
 //@desc test is it work??
 //@route GET /users/get
@@ -10,12 +11,10 @@ const userModel = require("../models/userModel");
 const getData = async (req, res) => {
   try {
     const getResult = await userModel.find({});
-    console.log(getResult);
-    res.status(200).json(getResult);
+    res.status(200).json(createResponse(true, getResult, "success"));
   } catch (error) {
     console.log("error", error);
-
-    res.status(500).json({ message: "something went wrong" });
+    res.status(500).json(createResponse(false, null, "something went wrong"));
   }
 };
 
@@ -28,13 +27,14 @@ const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-      return res.status(400).json({ message: "all fields are mandatory" });
+      // return res.status(400).json({ message: "all fields are mandatory" });
+      return res.status(400).json(createResponse(false, null, "all fields are mandatory"));
     }
 
     const userAvailable = await userModel.findOne({ $or: [{ email }, { name }] });
 
     if (userAvailable) {
-      return res.status(400).json({ message: "user already registered" });
+      return res.status(400).json(createResponse(false, null, "user already registered"));
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -48,13 +48,15 @@ const registerUser = async (req, res) => {
     console.log(`user created ${user}`);
 
     if (user) {
-      return res.status(201).json({ _id: user.id, email: user.email });
+      return res
+        .status(201)
+        .json(createResponse(true, user, "registration completed successfully"));
     } else {
-      return res.status(400).json({ message: "user data is not valid" });
+      return res.status(400).json(createResponse(false, null, "user data is not valid"));
     }
   } catch (error) {
     console.log("error", error);
-    return res.sendStatus(400);
+    res.status(400).json(createResponse(false, null, "something went wrong"));
   }
 };
 
@@ -67,13 +69,13 @@ const loginUser = async (req, res) => {
     const { name, password } = req.body;
 
     if (!name || !password) {
-      return res.status(400).json({ message: "all fileds are mandatory" });
+      return res.status(400).json(createResponse(false, null, "all fields are mandatory"));
     }
 
     const user = await userModel.findOne({ name });
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(404).json({ message: "wrong password or name" });
+      return res.status(404).json(createResponse(false, null, "wrong password or name"));
     }
 
     if (user && (await bcrypt.compare(password, user.password))) {
@@ -88,11 +90,11 @@ const loginUser = async (req, res) => {
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: "1h" }
       );
-      return res.status(200).json({ accessToken });
+      res.status(200).json(createResponse(true, accessToken, "the token is generated"));
     }
   } catch (error) {
     console.log("error", error);
-    return res.sendStatus(404);
+    res.status(400).json(createResponse(false, null, "something went wrong"));
   }
 };
 
