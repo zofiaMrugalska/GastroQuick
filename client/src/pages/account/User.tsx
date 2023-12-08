c3c73f41f4f11ed86ab851c4c99ed4dca54e9dbb
Zosia <zosiagrafika11@gmail.com>
2023-12-08 20:38:31 +0100

    user page style

--- client/src/pages/account/User.tsx
+++ client/src/pages/account/User.tsx
@@ -8,11 +8,14 @@ import {
 } from "../../interfaces/CartInterfaces";
 import { PlacingOrderServices } from "../../services/PlacingOrderServices";
 import formatDate from "../../utils/dateUtils";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 //to codereview
 
 const User = () => {
   const [sendedOrders, setSendedOrder] = useState<ResponseOrderDataInterface[]>();
+  const [openOrder, setOpenOrder] = useState<boolean>(true);
+  const [selectedOrder, setSelectedOrder] = useState();
   const isAuthenticated = useAuthCheck();
 
   const getOrders = async () => {
@@ -31,9 +34,15 @@ const User = () => {
     }
   }, []);
 
+  const toggleOrderInfo = (sendedOrderId: any): void => {
+    setOpenOrder(!openOrder);
+    setSelectedOrder(sendedOrderId);
+  };
+
   return (
-    <div>
+    <div className=" ml-10 mt-20">
       {sendedOrders?.map((orderDetails: ResponseOrderDataInterface) => {
+        console.log(sendedOrders);
         let order = orderDetails.order;
         const summaryOrder = order.reduce((summary: number, orderItem: ExtendOrderInterface) => {
           let total = summary + (orderItem.price || 0);
@@ -41,47 +50,77 @@ const User = () => {
         }, 0);
 
         return (
-          <div key={orderDetails._id} className="mt-10 ">
-            <p>{formatDate(new Date(orderDetails.createdAt))}</p>
-
-            <div
-              className="grid grid-cols-1
-          xl:grid-cols-3"
-            >
-              <div>
-                <p>name: {orderDetails.name}</p>
-                <p>surname: {orderDetails.surname}</p>
-                <p>phone number: {orderDetails.phoneNumber}</p>
-              </div>
-
-              <div>
-                <p>city: {orderDetails.city}</p>
-                <p>street: {orderDetails.street}</p>
-                <p>house number: {orderDetails.houseNumber}</p>
-                <p>payment method: {orderDetails.paymentMethod}</p>
-              </div>
-
-              <div>
-                {orderDetails.order?.map((order: ExtendOrderInterface) => {
-                  return (
-                    <div key={order._id}>
-                      <div className="flex gap-5">
-                        <p>{order.meal?.name}</p>
-
-                        <div className="flex">
-                          <p>{order.quantity}</p>
-                          <p>x</p>
-                          <p>{order.meal?.price}</p>
-                        </div>
-
-                        <p>{order.price}</p>
-                      </div>
-                    </div>
-                  );
-                })}
-                <p>Total Price: {summaryOrder}</p>
-              </div>
+          <div key={orderDetails._id} className="mt-5 p-5 rounded-lg shadow-shadowInset ">
+            <div className="flex items-center gap-2">
+              <p className=" text-xl font-semibold">
+                {formatDate(new Date(orderDetails.createdAt))}
+              </p>
+              <button onClick={() => toggleOrderInfo(orderDetails._id)}>
+                {openOrder && orderDetails._id === selectedOrder ? (
+                  <IoMdArrowDropup size={22} />
+                ) : (
+                  <IoMdArrowDropdown size={22} />
+                )}
+              </button>
             </div>
+
+            {openOrder && orderDetails._id === selectedOrder ? (
+              <div
+                className="grid grid-cols-1
+          lg:grid-cols-3"
+              >
+                <div className=" mb-3">
+                  <p>
+                    <span className="font-bold">name:</span> {orderDetails.name}
+                  </p>
+                  <p>
+                    <span className="font-bold">surname:</span> {orderDetails.surname}
+                  </p>
+                  <p>
+                    <span className="font-bold">phone number: </span>
+                    {orderDetails.phoneNumber}
+                  </p>
+                </div>
+
+                <div className=" mb-3">
+                  <p>
+                    <span className="font-bold">city:</span> {orderDetails.city}
+                  </p>
+                  <p>
+                    <span className="font-bold">street:</span> {orderDetails.street}
+                  </p>
+                  <p>
+                    <span className="font-bold">house number:</span> {orderDetails.houseNumber}
+                  </p>
+                  <p>
+                    <span className="font-bold">payment method:</span> {orderDetails.paymentMethod}
+                  </p>
+                </div>
+
+                <div>
+                  {orderDetails.order?.map((order: ExtendOrderInterface) => {
+                    return (
+                      <div key={order._id}>
+                        <div className="grid grid-cols-4 md:flex md:gap-5 lg:grid lg:grid-cols-4">
+                          <p className="col-span-2">{order.meal?.name}</p>
+
+                          <div className="flex">
+                            <p>{order.quantity}</p>
+                            <p>x</p>
+                            <p>{order.meal?.price}</p>
+                          </div>
+
+                          <p>{order.price}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                  <p className="mt-1 text-lg font-semibold">Total Price: {summaryOrder}$</p>
+                </div>
+              </div>
+            ) : (
+              <p className="mt-1 text-lg font-semibold">Total Price: {summaryOrder}$</p>
+            )}
           </div>
         );
       })}
